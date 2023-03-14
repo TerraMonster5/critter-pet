2a11aa8836f64e459a6ed22f796aef7ca17728b5
TerraMonster5 <william.dargan@gmail.com>
2023-03-14 19:04:19 +0000

    Added gitignore and adjusted values

--- src/critter.py
+++ src/critter.py
@@ -1,13 +1,21 @@
 import tkinter as tk
+'''
+Todo:
+- Add file saving
+- Add away time calculation
+
 from datetime import datetime
+'''
 
 class Critter(tk.Tk):
     __total = 0
 
+    # Getter for number of instantiated Critters
     @staticmethod
     def getNumberCritters() -> int:
         return Critter.__total
 
+    # Initialise each object once instantiated
     def __init__(self, name: str) -> None:
         super().__init__()
 
@@ -17,37 +25,53 @@ class Critter(tk.Tk):
         self._hunger = 0
         self._boredom = 0
 
+        # Setup window
         self.title(f"{self._name} the Critter")
-        self.resizable(0, 0)
+        self.resizable(False, False)
         self.geometry("300x300")
 
-        self._status = tk.Label(self, text="Status Unknown", font=("Ariel", 15))
-        self._status.pack(expand=True)
-        self._status.after(1000, self._update)
+        # Setup status label
+        self._statusLb = tk.Label(self, text="Status Unknown", font=("Ariel", 15))
+        self._statusLb.pack(pady=(100, 0))
 
+        self._unhappinessLb = tk.Label(self, text="", font=("Ariel", 10))
+        self._unhappinessLb.pack()
+
+        self.after(2000, self._update)
+
+        # Increment number of instatiated Critters once fully initialised
         Critter.__total += 1
 
+    # Increment stats every 10 seconds
     def _update(self) -> None:
         self._hunger += 1
         self._boredom += 1
 
-        self._status.after(1000, self._update)
+        self._statusLb.configure(text=f"Hunger: {self._hunger} | Boredom: {self._boredom}")
+        self._unhappinessLb.configure(text=f"Happiness: {self.mood}")
+
+        self.after(2000, self._update)
 
     @property
-    def _mood(self) -> str:
+    def mood(self) -> str:
         unhappiness = self._hunger + self._boredom
-        if unhappiness <= 5:
+        if unhappiness <= 10:
             m = "happy"
-        elif 5 < unhappiness <= 10:
+        elif 10 < unhappiness <= 20:
             m = "okay"
-        elif 10 < unhappiness <= 15:
+        elif 20 < unhappiness <= 30:
             m = "frustrated"
         else:
             m = "mad"
         return m
     
-    def talk(self) -> None:
-        return self._mood
+    def eat(self) -> None:
+        self._hunger -= 8
+        if self._hunger < 0: self._hunger = 0
+    
+    def play(self) -> None:
+        self._boredom -= 8
+        if self._boredom < 0: self._boredom = 0
 
 if __name__ == "__main__":
     a = Critter("Bob")
